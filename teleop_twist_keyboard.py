a5d568a1d4e17fc8ad0cce8915a26c8baf7e48ba
Andy Ponce <andy.ponce@outlook.com>
2022-09-20 04:08:54 +0000

    Commented out certain functionality of teleop_twist_keyboard for testing

--- teleop_twist_keyboard.py
+++ teleop_twist_keyboard.py
@@ -72,33 +72,33 @@ CTRL-C to quit
 
 moveBindings = {
     'i': (1, 0, 0, 0),
-    'o': (1, 0, 0, -1),
+    # 'o': (1, 0, 0, -1),
     'j': (0, 0, 0, 1),
     'l': (0, 0, 0, -1),
-    'u': (1, 0, 0, 1),
+    # 'u': (1, 0, 0, 1),
     ',': (-1, 0, 0, 0),
-    '.': (-1, 0, 0, 1),
-    'm': (-1, 0, 0, -1),
-    'O': (1, -1, 0, 0),
-    'I': (1, 0, 0, 0),
-    'J': (0, 1, 0, 0),
-    'L': (0, -1, 0, 0),
-    'U': (1, 1, 0, 0),
-    '<': (-1, 0, 0, 0),
-    '>': (-1, -1, 0, 0),
-    'M': (-1, 1, 0, 0),
-    't': (0, 0, 1, 0),
-    'b': (0, 0, -1, 0),
+    # '.': (-1, 0, 0, 1),
+    # 'm': (-1, 0, 0, -1),
+    # 'O': (1, -1, 0, 0),
+    # 'I': (1, 0, 0, 0),
+    # 'J': (0, 1, 0, 0),
+    # 'L': (0, -1, 0, 0),
+    # 'U': (1, 1, 0, 0),
+    # '<': (-1, 0, 0, 0),
+    # '>': (-1, -1, 0, 0),
+    # 'M': (-1, 1, 0, 0),
+    # 't': (0, 0, 1, 0),
+    # 'b': (0, 0, -1, 0),
 }
 
-speedBindings = {
-    'q': (1.1, 1.1),
-    'z': (.9, .9),
-    'w': (1.1, 1),
-    'x': (.9, 1),
-    'e': (1, 1.1),
-    'c': (1, .9),
-}
+# speedBindings = {
+#     'q': (1.1, 1.1),
+#     'z': (.9, .9),
+#     'w': (1.1, 1),
+#     'x': (.9, 1),
+#     'e': (1, 1.1),
+#     'c': (1, .9),
+# }
 
 
 def getKey(settings):
@@ -125,8 +125,8 @@ def restoreTerminalSettings(old_settings):
     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
 
 
-def vels(speed, turn):
-    return 'currently:\tspeed %s\tturn %s ' % (speed, turn)
+# def vels(speed, turn):
+#     return 'currently:\tspeed %s\tturn %s ' % (speed, turn)
 
 
 def main():
@@ -137,32 +137,32 @@ def main():
     node = rclpy.create_node('teleop_twist_keyboard')
     pub = node.create_publisher(geometry_msgs.msg.Twist, 'cmd_vel', 10)
 
-    speed = 0.5
-    turn = 1.0
+    #speed = 0.5
+    #turn = 1.0
     x = 0.0
     y = 0.0
     z = 0.0
     th = 0.0
-    status = 0.0
+    #status = 0.0
 
     try:
         print(msg)
-        print(vels(speed, turn))
+        #print(vels(speed, turn))
         while True:
             key = getKey(settings)
             if key in moveBindings.keys():
-                x = moveBindings[key][0]
-                y = moveBindings[key][1]
-                z = moveBindings[key][2]
-                th = moveBindings[key][3]
-            elif key in speedBindings.keys():
-                speed = speed * speedBindings[key][0]
-                turn = turn * speedBindings[key][1]
+                x = float(moveBindings[key][0])
+                y = float(moveBindings[key][1])
+                z = float(moveBindings[key][2])
+                th = float(moveBindings[key][3])
+            # elif key in speedBindings.keys():
+            #     speed = speed * speedBindings[key][0]
+            #     turn = turn * speedBindings[key][1]
 
-                print(vels(speed, turn))
-                if (status == 14):
-                    print(msg)
-                status = (status + 1) % 15
+            #     print(vels(speed, turn))
+            #     if (status == 14):
+            #         print(msg)
+            #     status = (status + 1) % 15
             else:
                 x = 0.0
                 y = 0.0
@@ -172,12 +172,12 @@ def main():
                     break
 
             twist = geometry_msgs.msg.Twist()
-            twist.linear.x = x * speed
-            twist.linear.y = y * speed
-            twist.linear.z = z * speed
+            twist.linear.x = x #* speed
+            twist.linear.y = y #* speed
+            twist.linear.z = z #* speed
             twist.angular.x = 0.0
             twist.angular.y = 0.0
-            twist.angular.z = th * turn
+            twist.angular.z = th #* turn
             pub.publish(twist)
 
     except Exception as e:
